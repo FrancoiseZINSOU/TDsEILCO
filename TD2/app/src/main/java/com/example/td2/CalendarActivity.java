6c4e084903c97f441e369d31a8af32ccef2e93d7
Francoise ZINSOU <zinsousabine7@gmail.com>
2021-12-07 14:27:30 +0100

    TD2 : Menu et Meteo
    TD3 : Dice

---
+++ TD2/app/src/main/java/com/example/td2/CalendarActivity.java
@@ -0,0 +1,14 @@
+package com.example.td2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CalendarActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calendar);
+    }
+}
---
+++ TD2/app/src/main/java/com/example/td2/PasswordActivity.java
@@ -0,0 +1,14 @@
+package com.example.td2;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PasswordActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.password);
+    }
+}
